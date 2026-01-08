acd109b9846c1ae4d78b09058fb305fc6d227107
Chris Petersen <chris@petersen.io>
2026-01-08 08:36:25 -0800

    Changing out the tooling to be ruby based

---
+++ Gemfile
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+source 'https://rubygems.org'
+
+gem 'csv', '~> 3.3'  # Required for Ruby 3.4+
+gem 'thor', '~> 1.3'
--- interactive-asciidoctor-shell.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-# Docker image: https://hub.docker.com/r/asciidoctor/docker-asciidoctor
-# Docker image repository: https://github.com/asciidoctor/docker-asciidoctor
-
-docker run -it -v $(pwd)/book:/documents/ asciidoctor/docker-asciidoctor
